736f3f4ce43ca9bf623540986e4f3b54559579ec
razimbrao <rafaelozimbrao@gmail.com>
2025-03-10 22:28:28 -0300

    Revert "adiciona implementação de atuadores e sensores com MQTT, atualiza Dockerfile e configurações do docker-compose"
    This reverts commit fb38a13c38bbdd214ae8bd4380316229ddcfd540.

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,4 @@ RUN apt-get update && apt-get install -y \
     && rm -rf /var/lib/apt/lists/*
 
 RUN pip install --no-cache-dir paho-mqtt
-CMD ["python", "sensors/sensors.py"]
+CMD ["python", "./sensors.py"]
--- actuators.py
+++ actuators.py
@@ -1,13 +1,11 @@
 import json
 import paho.mqtt.client as mqtt
-import time
 
 # Lista de nós do cluster
 BROKERS = ["rabbitmq1", "rabbitmq2", "rabbitmq3"]
 BROKER_PORT = 1883
-TOPIC = "actuators.command" 
-STATE_TOPIC = "actuators.state"
-RETRY_INTERVAL = 5
+TOPIC = "actuators/command" 
+STATE_TOPIC = "actuators/state"
 
 class Actuators:
     def __init__(self):
@@ -17,27 +15,25 @@ class Actuators:
         self.connect()
 
     def connect(self):
-        while True:
-            for broker in BROKERS:
-                try:
-                    self.client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
-                    self.client.on_message = self.on_message  # Define o callback para mensagens recebidas
-                    self.client.username_pw_set("admin", "admin123")
-                    self.client.connect(broker, BROKER_PORT, 60)
-                    self.current_broker = broker
-                    print(f"Conectado ao nó {broker}")
+        for broker in BROKERS:
+            try:
+                self.client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+                self.client.on_message = self.on_message  # Define o callback para mensagens recebidas
 
-                    # Assina o tópico de comandos
-                    self.client.subscribe(TOPIC, qos=2)
-                    print(f"Inscrito no tópico {TOPIC}")
+                self.client.connect(broker, BROKER_PORT, 60)
+                self.current_broker = broker
+                print(f"Conectado ao nó {broker}")
 
-                    self.publish_state()
-                    return
-                except Exception as e:
-                    print(f"Falha na conexão com {broker}: {str(e)}")
-            
-            print(f"Todas as conexões falharam. Tentando novamente em {RETRY_INTERVAL} segundos...")
-            time.sleep(RETRY_INTERVAL)
+                # Assina o tópico de comandos
+                self.client.subscribe(TOPIC, qos=2)
+                print(f"Inscrito no tópico {TOPIC}")
+
+                self.publish_state()
+                return
+            except Exception as e:
+                print(f"Falha na conexão com {broker}: {str(e)}")
+
+        raise Exception("Nenhum nó do cluster disponível")
 
     def on_message(self, client, userdata, msg):
         """Callback para processar mensagens recebidas."""
